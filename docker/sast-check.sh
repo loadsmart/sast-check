76d64e0971a358f24168efcc8a901a901ec3298f
andre <andregus@gmail.com>
2021-01-29 15:44:13 -0300

    16 - Trying to fix missing ] on shellscript

--- docker/sast-check.sh
+++ docker/sast-check.sh
@@ -18,14 +18,15 @@ bandit -r -a vuln -ii -ll -x .git,.svn,.mvn,.idea,dist,bin,obj,backup,docs,tests
 # Print Report on screen to developers
 #cat "${TMP_REPORT}"
 
-if [ -z ${DD_CLIENT_API_KEY}] || [ -z ${GITHUB_REPOSITORY} ]; then
+if [ -z ${DD_CLIENT_API_KEY}] || [ -z ${GITHUB_REPOSITORY} ]
+then
   echo "\$DD_CLIENT_API_KEY or \$SGITHUB_REPOSITORY are empty. I can't send metrics to DataDog without this information!"
 else
-  CONFIDENCE_HIGH=`cat ${TMP_REPORT} | jq -r '.metrics._totals."CONFIDENCE.HIGH"'`
-  CONFIDENCE_MEDIUM=`cat ${TMP_REPORT} | jq -r '.metrics._totals."CONFIDENCE.MEDIUM"'`
-  SEVERITY_HIGH=`cat ${TMP_REPORT} | jq -r '.metrics._totals."SEVERITY.HIGH"'`
-  SEVERITY_MEDIUM=`cat ${TMP_REPORT} | jq -r '.metrics._totals."SEVERITY.MEDIUM"'`
-  LOC=`cat ${TMP_REPORT} | jq -r '.metrics._totals.loc'`
+  CONFIDENCE_HIGH=$(cat ${TMP_REPORT} | jq -r '.metrics._totals."CONFIDENCE.HIGH"')
+  CONFIDENCE_MEDIUM=$(cat ${TMP_REPORT} | jq -r '.metrics._totals."CONFIDENCE.MEDIUM"')
+  SEVERITY_HIGH=$(cat ${TMP_REPORT} | jq -r '.metrics._totals."SEVERITY.HIGH"')
+  SEVERITY_MEDIUM=$(cat ${TMP_REPORT} | jq -r '.metrics._totals."SEVERITY.MEDIUM"')
+  LOC=$(cat ${TMP_REPORT} | jq -r '.metrics._totals.loc')
 fi
 
 # Removing temporary files
